8098fd09521e91211962849d3268b9a2622a707c
thomas <zhangfan@thomas-fan.com>
2021-02-09 16:02:33 +0800

    完成最长前缀解法 1

---
+++ 14_longest_common_prefix/longest_common_prefix_test.go
@@ -0,0 +1,26 @@
+package longest_common_prefix
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type testCase struct {
+	Words []string
+	Wanted string
+}
+
+var cases = []testCase{
+	{[]string{"flower", "flight", "flow"}, "fl"},
+	{[]string{"dog", "racecar", "car"}, ""},
+	{[]string{"a"}, "a"},
+	{[]string{"", "b"}, ""},
+	{[]string{"c", "acc", "ccc"}, ""},
+}
+
+func TestLongestCommonPrefix1(t *testing.T) {
+	for _, item := range cases {
+		got := LongestCommonPrefix1(item.Words)
+		assert.Equal(t, item.Wanted, got)
+	}
+}
