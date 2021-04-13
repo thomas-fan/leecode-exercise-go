97d60f9d66dc69f54cc78c407a4bea24fe5ffce6
thomas <zhangfan@thomas-fan.com>
2021-04-13 15:54:01 +0800

    完成第四题,中位数

--- 4_median_of_two_sorted_arrays/median_of_two_sorted_arrays.go
+++ 4_median_of_two_sorted_arrays/median_of_two_sorted_arrays.go
@@ -1,10 +1,10 @@
 package median_of_two_sorted_arrays
 
-import (
-	"sort"
-)
+import "sort"
 
 //给定两个大小为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的中位数。
+
+//解法 1,暴力合并两数组,根据奇偶求出中位数, 时间复杂度O(m+n)
 func findMedianSortedArrays1(nums1 []int, nums2 []int) float64 {
 	for _, num := range nums2 {
 		nums1 = append(nums1, num)
@@ -12,9 +12,6 @@ func findMedianSortedArrays1(nums1 []int, nums2 []int) float64 {
 
 	sort.Ints(nums1)
 	length := len(nums1)
-	if length <= 0 {
-		return 0.0
-	}
 	if length == 1 {
 		return float64(nums1[0])
 	}
